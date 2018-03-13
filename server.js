336dd769f16f1201bb5d656bb0f7fab96211ff75
derek-haworth <derekhaw1@gmail.com>
2018-03-12 19:22:45 -0500

    set up js files

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+// =============================================================
+var express = require('express');
+var exphbs = require('express-handlebars');;
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ 
+    defaultLayout: "main" 
+  })
+);
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+    console.log("I\'m listening... on port " + PORT);
+})
