39a52f71913b54f21131fe8c16955617ac030cf7
Noah Strayve <noahstrayve@gmail.com>
2018-03-19 17:48:15 -0500

    added card for reviews

--- models/category.js
+++ models/category.js
@@ -1,37 +1,24 @@
 module.exports= function(sequelize, DataTypes) {
-
-	var Category = sequelize.define("Category", {
-
-		restaurant: {
-			type: DataTypes.STRING	
-						
-		},
-        
-        museum: {
-            type: DataTypes.STRING
-            				
-		},
-        
-        shop: {
-			type: DataTypes.STRING
-			
-        },
-
-        bar: {
-            type: DataTypes.STRING
-
-        },       
-
-
+	var Category = sequelize.define("categories", {
+		category_name: {
+	    	type: DataTypes.STRING,
+	    	allowNull: false,
+	    	validation: {
+	    		len: [1]
+	    	}
+	    },
+	    short_description: {
+	    	type: DataTypes.STRING
+	    },
     });
 
     Category.associate = function(models) {
         // Associating Category with Places
         // When an Category is deleted, also delete any associated Places
-        Category.hasMany(models.Place, {
+        Category.belongsTo(models.places, {
           onDelete: "cascade"
         });
-      };
+    };
 
     return Category;
 
--- models/place.js
+++ models/place.js
@@ -1,42 +1,24 @@
 module.exports= function(sequelize, DataTypes) {
 
-	var Place = sequelize.define("Place", {
-
+	var Place = sequelize.define("places", {
 		name: {
-			type: DataTypes.STRING	
-						
+			type: DataTypes.STRING				
 		},
-        
         address: {
             type: DataTypes.STRING
-            				
-		},
-        
-        category: {
-			type: DataTypes.STRING
-			
-        },
-
-        comparedTo: {
-            type: DataTypes.STRING
-
-        },  
-        
+		},      
         review: {
             type: DataTypes.TEXT
         }
-
-
     });
 
     Place.associate = function(models) {
-        
-        Place.hasOne(models.Category, {
-            foreignKey: {
-                allowNull: false
-              }
-        });
-      };
+      	Place.hasOne(models.categories, {
+        	foreignKey: {
+            	allowNull: false
+            }
+      	});
+    };
 
     return Place;
 
